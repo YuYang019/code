89e68186577851378cc701a487df12a8b18e3ee6
maoyuyang <maoyuyang@baidu.com>
2024-07-19 21:55:57 +0800

    commit

--- 请求合并.js
+++ 请求合并.js
@@ -26,6 +26,11 @@ async function test3() {
 // 2、究竟什么时候发起请求，合并之后怎么处理之前的请求
 // 3、对于 test3，怎么做参数过滤？
 
+// 解决思路
+// 1、以 id 为粒度判断和存储，而不是数组维度
+// 2、不马上请求，而是用一个数组存储 参数，reoslve, reject，下一个 tick 触发请求
+// 3、使用缓存, id => {id}，每次需要判断，请求参数是否在缓存里，如果存在，则不加入当前参数列表
+
 let timer = null
 const requestIds = []
 const pendingTasks = []
